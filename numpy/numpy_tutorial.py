127f7b9c4eb45b73e21450e282d427fa145e4883
Tariqul Islam <cavidpark@gmail.com>
2021-07-31 21:20:21 +0600

    numpy

---
+++ numpy/numpy_tutorial.py
@@ -0,0 +1,19 @@
+import numpy as np
+import time
+import sys
+SIZE = 1000000
+l1 = range(SIZE)
+l2 = range(SIZE)
+
+a1 = np.arange(SIZE)
+a2 = np.arange(SIZE)
+
+start = time.time()
+list_result = [(x+y) for x,y in zip(l1,l2)]
+print("python list took :",(time.time()-start) * 1000)
+# print(list_result)
+
+np_time = time.time()
+numpy_result = a1+a2
+print("time of np_result :", (time.time()-np_time) * 1000)
+# print(numpy_result)
