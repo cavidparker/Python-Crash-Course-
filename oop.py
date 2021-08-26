385c61e5b94cbdf40452e9617e330b5dbd6f48c5
Tariqul Islam <cavidpark@gmail.com>
2021-08-27 01:43:01 +0600

    added python fun project

---
+++ oop.py
@@ -0,0 +1,14 @@
+class Computer:
+    def config(self):
+        print("i5","16gb","1TB")
+
+    def model(self):
+        print(2021)        
+
+
+
+# Comp = Computer()
+# Computer.config(Comp)
+
+modl = Computer()
+modl.config()
